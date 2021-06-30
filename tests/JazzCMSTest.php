dfe172d862cedde68679ce6429e3c0f5a9d5add9
Naveed Shahzad <lhrciit@gmail.com>
2021-06-30 19:17:21 +0500

    fixing the yml files try-1

---
+++ tests/JazzCMSTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by Naveed Shahzad.
+ * User: naveed
+ * Date: 30/06/2021
+ * Time: 6:39 PM
+ */
+
+namespace NsTechNs\JazzCMS\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class JazzCMSTest extends TestCase
+{
+    /** @test */
+    public function can_we_send_sms()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+        $this->assertEquals('admin', "admin");
+    }
+}
